1b7b24bc17f014f9226b26e19a9f7eca068e2a99
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-11-09 17:01:05 +0900

    アップロードボタンをComponentに切り出した。

--- frontend/src/src/App.tsx
+++ frontend/src/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UploadButton from './components/upload_button';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <h1>Upload your Image</h1>
       <h2>File should be Jpeg.</h2>
 
-      <button>Choose a file</button>
+      <UploadButton />
     </div>
   );
 }
---
+++ frontend/src/src/components/upload_button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class UploadButton extends React.Component {
+    render() {
+        return (
+            <button>Choose a file.</button>
+        )
+    }
+}
+
+export default UploadButton;
