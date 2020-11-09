bbed64a72a12bfb1531fabe000520d4358286419
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-11-09 16:47:39 +0900

    とりあえずアップロード画面をザックリと。

--- frontend/src/src/App.tsx
+++ frontend/src/src/App.tsx
@@ -5,20 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Upload your Image</h1>
+      <h2>File should be Jpeg.</h2>
+
+      <button>Choose a file</button>
     </div>
   );
 }
